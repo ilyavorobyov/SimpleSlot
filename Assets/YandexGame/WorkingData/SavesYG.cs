5052f1d15561053a12e8ac9bc49beb4a2167294d
ilyavorobyov <iilyavorobyov@yandex.ru>
2024-11-28 22:37:37 +0300

    Add cloud saves, add rewarded adv

---
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -0,0 +1,21 @@
+﻿
+namespace YG
+{
+    [System.Serializable]
+    public class SavesYG
+    {
+        // "Технические сохранения" для работы плагина (Не удалять)
+        public int idSave;
+        public bool isFirstSession = true;
+        public string language = "ru";
+        public bool promptDone;
+
+        // Свои данные: 
+        public int Balance = 3000;
+        public int BetValue = 10;
+        public bool IsFirstStart = true;
+        public SavesYG()
+        {
+        }
+    }
+}
